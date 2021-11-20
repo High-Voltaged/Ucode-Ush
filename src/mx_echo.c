bc7cee040fb5900faffbcdda3505c706eb7eac49
Yaroslav Chuiko <ychuiko@c1r4p6.khpi.ucode-connect.study>
2021-11-20 19:03:24 +0200

    edded echo -e(-E)

--- src/mx_echo.c
+++ src/mx_echo.c
@@ -24,6 +24,80 @@ void mx_echo_add_flag(t_echo_flags** flags, char flag) {
 
 }
 
+void print_bec(char *str, int *letter_count) {
+        // wchar_t c = L'\U0001f602';
+
+    switch (str[2])
+    {
+    case 'a':
+        mx_printchar('\a');
+        (*letter_count) += 2;
+        break;
+    
+    case 'b':
+        mx_printchar('\b');
+        (*letter_count) += 2;
+        break;
+    
+    case 'c':
+        (*letter_count) += mx_strlen(str);
+        break;
+
+    case 'e':
+        // mx_printchar('\e');
+        mx_printchar(27);
+        (*letter_count) += 2;
+        break;
+
+    case 'f':
+        mx_printchar('\f');
+        (*letter_count) += 2;
+        break;
+    
+    case 'n':
+        // printf("%lc", c);
+        mx_printchar('\n');
+        (*letter_count) += 2;
+        break;
+    
+    case 't':
+        mx_printchar('\t');
+        (*letter_count) += 2;
+        break;
+
+    case 'v':
+        mx_printchar('\v');
+        (*letter_count) += 2;
+        break;
+    
+    default:
+        mx_printchar('\\');
+        (*letter_count) += 1;
+        break;
+    }
+
+}
+
+void out_echo_e(char *str)
+{
+    for (int i = 0; i < mx_strlen(str); i++)
+    {
+        if (str[i] == '\\' && str[i + 1] != '\\')
+        {
+            continue;
+        }
+        else if (str[i] == '\\' && str[i + 1] == '\\')
+        {
+            print_bec(str + i, &i);
+        }
+        else
+        {
+            mx_printchar(str[i]);
+        }
+    }
+    
+}
+
 int mx_echo(t_cmd_utils* utils)
 {
     int flag_count = 0;
@@ -32,12 +106,11 @@ int mx_echo(t_cmd_utils* utils)
 
     for (int i = 1 + flag_count; utils->args[i]; i++)
     {
-        mx_printstr(utils->args[i]);
+        flags->e ? out_echo_e(utils->args[i]) : mx_printstr(utils->args[i]);
         utils->args[i + 1] ? mx_printstr(" ") : (void) 0;
     }
 
     flags->n ? (void) 0 : mx_printstr("\n");
-    
 
     return 0;
 }
