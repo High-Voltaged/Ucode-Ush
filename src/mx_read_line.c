4f48b86c49872282c77644271373c1fc27009fb8
Yaroslav Chuiko <ychuiko@c1r4p6.khpi.ucode-connect.study>
2021-12-08 17:56:32 +0200

    mod mx_read_line.c, added CTRL D handler

--- src/mx_read_line.c
+++ src/mx_read_line.c
@@ -1,62 +1,100 @@
 #include "../inc/ush.h"
 
-int mx_getch(void)
+static int mx_getch(void)
 {
     struct termios oldattr, newattr;
     int ch;
-    tcgetattr( STDIN_FILENO, &oldattr );
+    tcgetattr(STDIN_FILENO, &oldattr);
     newattr = oldattr;
-    newattr.c_lflag &= ~( ICANON | ECHO );
-    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    newattr.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
     ch = getchar();
-    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
     return ch;
 }
 
-char** mx_read_line(/*t_cmd_utils* utils*/) {
+static void handle_arrows()
+{
+    mx_getch(); // skip the [
+    switch (mx_getch())
+    { // the real value
+    case 'A':
+        // code for arrow up
+        break;
+    case 'B':
+        // code for arrow down
+        break;
+    case 'C':
+        // code for arrow right
+        break;
+    case 'D':
+        // code for arrow left
+        break;
+    }
+}
 
-    char* line = NULL;
-    size_t length = 0;
-
-    if (getline(&line, &length, stdin) == -1) {
-
-        free(line);
-        exit(1);
-
+static int handle_keycode(char **line, int *pos, char keycode)
+{
+    if (keycode == '\033') // if the first value is esc
+    {
+        handle_arrows();
+    }
+    else if (keycode == MX_ENTER)
+    {
+        mx_printchar('\n');
+        return 1;
+    }
+    else if (keycode == MX_CTRL_C)
+    {
+        for (int i = 0; i < mx_strlen(*line); i++)
+        {
+            (*line)[i] = '\0';
+        }
+        mx_printchar('\n');
+        return 1;
+    }
+    else if (keycode == MX_CTRL_D)
+    {
+        if ((*pos) == 0)
+        {
+            exit(EXIT_FAILURE);
+        }
+    }
+    else if (keycode == MX_BACKSPACE)
+    {
+        if ((*pos) > 0)
+        {
+            mx_printchar('\b');
+            mx_printchar(' ');
+            mx_printstr("\033[1D");
+            (*line)[--(*pos)] = '\0';
+        }
+    }
+    else
+    {
+        mx_printchar(keycode);
+        (*line)[(*pos)++] = keycode;
     }
 
-    // char s = 0;
-    // char buffer[BUFFER_LEN]; 
-    // int i = 0;
-    // FILE* fd = fopen("log.txt", "a");
-    // while ((s = getc(stdin))) {
+    return 0;
+}
 
-    //     if (s == 10)
-    //         break;
+char **mx_read_line()
+{
+    char *line = mx_strnew(1024);
+    char keycode = 0;
+    int pos = 0;
 
-        // if (s == 127) {
-        //     mx_printchar('\b');
-        //     mx_printchar(' ');
-        // }
+    while ((keycode = mx_getch()))
+    {
+        if (handle_keycode(&line, &pos, keycode))
+        {
+            break;
+        }
+    }
 
-    //     if (s == EOF) {
-    //         s = 0;
-    //         fprintf(fd, "eof entered\n");
-    //         if (i == 0) {
-    //             fprintf(fd, "Exit now...\n");
-    //             exit(EXIT_FAILURE);
-    //         }
-    //         // mx_getch();
-    //     }
-    //     mx_printchar(s);
-    //     buffer[i++] = s;
-
-    // }
-    // mx_printchar('\n');
-    // fclose(fd);
-    // buffer[i] = '\0';
-    char** separated = mx_strsplit(line, ';');
+    char **separated = mx_strsplit(line, ';');
     mx_strdel(&line);
-    return separated;
 
+    return separated;
 }
